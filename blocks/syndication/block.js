d70f725e9fb39b67f273e41778dff27d003314c2
Jan Boddez <jan.boddez@gmail.com>
2024-04-18 11:49:42 +0200

    Add prefix and suffix to Syndication block

--- blocks/syndication/block.js
+++ blocks/syndication/block.js
@@ -1,7 +1,8 @@
-( ( blocks, element, blockEditor, coreData, i18n ) => {
+( ( blocks, element, blockEditor, components, coreData, i18n ) => {
 	const { registerBlockType } = blocks;
 	const { createElement: el, RawHTML, useState } = element;
-	const { BlockControls, useBlockProps } = blockEditor;
+	const { BlockControls, InspectorControls, useBlockProps } = blockEditor;
+	const { PanelBody, Placeholder, ToggleControl, TextControl } = components;
 	const { useEntityRecord } = coreData;
 	const { __, sprintf } = i18n;
 
@@ -17,12 +18,14 @@
 				'</a>, ';
 		} );
 
-		/* translators: %s: plain-text "list" of links. */
-		return sprintf( __( 'Also on %s', 'indieblocks' ), output.replace( /[,\s]+$/, '' ) );
+		return output.replace( /[,\s]+$/, '' );
 	};
 
 	registerBlockType( 'indieblocks/syndication', {
 		edit: ( props ) => {
+			const prefix = props.attributes?.prefix ?? '';
+			const suffix = props.attributes?.suffix ?? '';
+
 			// We'd use `serverSideRender` but it doesn't support passing block
 			// context to PHP. I.e., rendering in JS better reflects what the
 			// block will look like on the front end.
@@ -33,14 +36,14 @@
 
 			const urls = [];
 
-			if ( '' !== mastodonUrl ) {
+			if ( mastodonUrl ) {
 				urls.push( {
 					name: __( 'Mastodon', 'indieblocks' ),
 					value: mastodonUrl,
 				} );
 			}
 
-			if ( '' !== pixelfedUrl ) {
+			if ( pixelfedUrl ) {
 				urls.push( {
 					name: __( 'Pixelfed', 'indieblocks' ),
 					value: pixelfedUrl,
@@ -51,12 +54,47 @@
 				'div',
 				useBlockProps(),
 				el( BlockControls ),
-				urls.length
-					? RawHTML( { children: render( urls ) } )
-					: props.context.postId
-					? __( 'No syndication links', 'indieblocks' )
-					: __( 'Syndication Links', 'indieblocks' )
+				props.isSelected
+					? el(
+							InspectorControls,
+							{ key: 'inspector' },
+							el(
+								PanelBody,
+								{
+									title: __( 'Prefix and Suffix', 'indieblocks' ),
+									initialOpen: true,
+								},
+								// @todo: Base these on "proper" `RichText` instances or something.
+								el( TextControl, {
+									label: __( 'Prefix', 'indieblocks' ),
+									value: prefix,
+									onChange: ( prefix ) => {
+										props.setAttributes( { prefix } );
+									},
+								} ),
+								el( TextControl, {
+									label: __( 'Suffix', 'indieblocks' ),
+									value: suffix,
+									onChange: ( suffix ) => {
+										props.setAttributes( { suffix } );
+									},
+								} )
+							)
+					  )
+					: null,
+				! props.context.postId
+					? __( 'Syndication Links', 'indieblocks' )
+					: urls.length
+					? RawHTML( { children: prefix + render( urls ) + suffix } )
+					: prefix + __( 'Syndication Links', 'indieblocks' ) + suffix
 			);
 		},
 	} );
-} )( window.wp.blocks, window.wp.element, window.wp.blockEditor, window.wp.coreData, window.wp.i18n );
+} )(
+	window.wp.blocks,
+	window.wp.element,
+	window.wp.blockEditor,
+	window.wp.components,
+	window.wp.coreData,
+	window.wp.i18n
+);
